b585c99b4b63401dfffae751b9b3deb262eb00e6
Larry Merino <merino17@iclud.com>
2023-07-27 01:39:25 +0200

    feat(installation script): added basic installation script

---
+++ install.zsh
@@ -0,0 +1,43 @@
+#!/bin/zsh
+
+echo "👉 Making sure if brew is installed on the system"
+if command -v brew &>/dev/null; then
+  echo "🍺 Brew already instaled"
+else
+  echo "Brew was not found on the system"
+  echo "🍺 Performing brew installation"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+echo
+echo "👉 Making sure if Rust is installed on the system"
+if command -v brew &>/dev/null; then
+  echo "🦀 Rust already instaled"
+else
+  echo "Rust was not found on the system"
+  echo "🦀 Performing rust installation"
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
+echo
+echo "🔧 Performing the installation of command line tools"
+
+echo
+echo "🔧 Installing neovim"
+brew install neovim
+
+echo
+echo "🔧 Installing exa"
+brew install exa
+
+echo
+echo "🔧 Installing bat"
+brew install bat
+
+# Installing esp-rs/esp-idf-template development dependencies
+echo
+echo "👉 Installing dependencies for esp-rs: std support"
+
+echo
+echo "💿 Installing libuv"
+brew install libuv
---
+++ zsh/.zshrc
@@ -0,0 +1,17 @@
+##
+# Aliases
+#
+
+# Git aliases
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gac='git add -A && git commit -m'
+
+# Exa aliases
+alias exal='exa --all --long --header --git --time modified --sort modified'
+
+# quick editor commands
+alias zshrc="$EDITOR ~/.zshrc"
